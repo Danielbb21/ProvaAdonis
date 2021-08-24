f12fbb7b7e9f7a55c5acf426cda2c11e4110ebfd
Daniel <danielbb2000@gmail.com>
2021-08-24 16:20:55 -0300

    adding verification when the numbers of game did not match with the specified game

--- Prova/app/Controllers/Http/GambleController.js
+++ Prova/app/Controllers/Http/GambleController.js
@@ -45,17 +45,6 @@ class GambleController {
 
       const data = request.input('data');
 
-
-      // const game = await Game.find(params.games_id);
-
-      // const gameNumbers = data.gameNumbers.toString();
-      // console.log(game['max-number'], data.gameNumbers.length )
-      // console.log('gamenubers', data.gameNumbers.length);
-      // if(game['max-number'] !==  data.gameNumbers.length){
-      //   throw new Error('mismatched game numbers')
-      // }
-
-      // const gamble = await Gamble.create({ gameNumbers, game_date: data.game_date,price: data.price, user_id: auth.user.id, game_id: params.games_id });
       const teste = data.map((element) => {
         return { ...element, gameNumbers: element.gameNumbers.toString(), user_id: auth.user.id }
       });
@@ -66,16 +55,28 @@ class GambleController {
       let teste2 = [];
 
       for (let i = 0; i < teste.length; i++) {
-        let teste3 = { ...teste[i], type: '', color: '' };
+        let teste3 = { ...teste[i], type: '', color: '', maxNumber: 0 };
 
         for (let j = 0; j < allGames.length; j++) {
           if (teste[i].game_id === allGames[j].id) {
             teste3.type = allGames[j].type
             teste3.color = allGames[j].color;
+            teste3.maxNumber = allGames[j]['max-number']
             teste2.push(teste3);
           }
         }
       }
+
+      teste2.map((game)=>{
+
+        const gameType = allGames.find(gm => gm.type === game.type);
+
+
+        if(gameType['max-number'] !== game.gameNumbers.split(',').length){
+          throw new Error('mismatched game numbers')
+        }
+      })
+
       const newGambles = teste2.map((gamble) => {
         return { ...gamble, price: gamble.price.toFixed(2).toString().replace('.', ','), date_game: formatDate(gamble.game_date) };
       })
